c540302f42dc21d38c365aabcb84d6991c7925e0
Graham <graham@whibse.com>
2017-04-11 14:25:04 -0700

    adds

---
+++ .emacs.d/elpa/seq-20151121.1017/seq-autoloads.el
@@ -0,0 +1,15 @@
+;;; seq-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("seq.el") (22104 54071 533579 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; seq-autoloads.el ends here
